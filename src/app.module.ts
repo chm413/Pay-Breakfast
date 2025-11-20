06b3f63f3285496ad684b941e0b2944e2fbdf31b
YNACh <146831697+chm413@users.noreply.github.com>
2025-11-20 10:12:23 +0800

    Update Pages artifact action version

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountsModule } from './accounts/accounts.module';
+import { AuthModule } from './auth/auth.module';
+import { ClassOrdersModule } from './class-orders/class-orders.module';
+import { RechargeModule } from './recharge/recharge.module';
+import * as entities from './entities';
+import { NotificationsModule } from './notifications/notifications.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT || 3306),
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'pay_breakfast',
+      charset: process.env.DB_CHARSET || 'utf8mb4_unicode_ci',
+      supportBigNumbers: true,
+      legacySpatialSupport: true,
+      dateStrings: true,
+      timezone: '+00:00',
+      entities: Object.values(entities),
+      synchronize: true,
+    }),
+    AccountsModule,
+    AuthModule,
+    ClassOrdersModule,
+    RechargeModule,
+    NotificationsModule,
+  ],
+})
+export class AppModule {}
